c18a08ca1aa0e141ef416dd9e67d4ddc22f146d9
Bo Chen <412862016@qq.com>
2018-09-15 19:40:22 +0800

    add getFlightRecordArrayListFromExcel function

--- src/problem/Problem.java
+++ src/problem/Problem.java
@@ -1,10 +1,91 @@
 package problem;
 
 import problem.component.FlightRecord;
+import problem.component.GatesInformation;
+import util.ioUtil.excel.excelReader;
 
 import java.util.ArrayList;
 
 public class Problem {
 
-    ArrayList<FlightRecord> flightRecordArrayList = new ArrayList<>();
+    protected ArrayList<FlightRecord> flightRecordArrayList
+            = new ArrayList<>();
+
+    protected ArrayList<GatesInformation> gatesInformationArrayList
+            = new ArrayList<>();
+
+    protected void getFlightRecordArrayListFromExcel() {
+
+        ArrayList<ArrayList<String>> originalData
+                = new ArrayList<>();
+
+        excelReader.importXlsFile(
+                "E:\\Java_Projects\\MCM\\resources\\InputData_2.xls",
+                0,true,
+                1, 306,
+                0, -1, originalData);
+
+        for (int i = 0; i < originalData.size(); i++) {
+
+            /*String arrivalDate = originalData.get(i).get(0);
+            String arrivalTime = originalData.get(i).get(1);*/
+            String arrivalDate = originalData.get(i).get(9).split(" +")[0];
+            String arrivalTime = originalData.get(i).get(9).split(" +")[1];
+            /*if (originalData.get(i).get(9).split(" ").length !=2) {
+                throw new RuntimeException();
+            }*/
+
+            String arrivalFlightName = originalData.get(i).get(2);
+            String arrivalType = originalData.get(i).get(3);
+            String planeType = originalData.get(i).get(4);
+            /*String leftDate = originalData.get(i).get(5);
+            String leftTime = originalData.get(i).get(6);*/
+            String leftDate = originalData.get(i).get(10).split(" +")[0];
+            String leftTime = originalData.get(i).get(10).split(" +")[1];
+            /*if (originalData.get(i).get(10).split(" ").length !=2) {
+                throw new RuntimeException();
+            }*/
+            String leftFlightName = originalData.get(i).get(7);
+            String leftType = originalData.get(i).get(8);
+
+            int arrival = convertDateToMinute(arrivalDate)
+                    + convertTimeToMinute(arrivalTime);
+            int left = convertDateToMinute(leftDate)
+                    + convertTimeToMinute(leftTime);
+
+            FlightRecord flightRecord = new FlightRecord(arrival, left,
+            arrivalFlightName, leftFlightName, arrivalType, leftType, planeType);
+            this.flightRecordArrayList.add(flightRecord);
+        }
+    }
+
+    private int convertDateToMinute(String date) {
+
+        String day = date.substring(date.length() - 2, date.length());
+
+        return Integer.valueOf(day) * 24 * 60;
+    }
+
+    private int convertTimeToMinute(String time) {
+
+        String[] splitResult = time.split(":");
+
+        if (splitResult.length != 2) {
+            throw new RuntimeException();
+        }
+
+        return (Integer.valueOf(splitResult[0].trim()) * 60
+                + Integer.valueOf(splitResult[1].trim()));
+    }
+
+
+    // Below is for test
+    public static void main(String[] args) {
+
+        Problem problem = new Problem();
+
+        problem.getFlightRecordArrayListFromExcel();
+
+        System.out.println("haha");
+    }
 }
